fa3e27964352fd17ab071d2db268f11b6bd06813
Niki Balajadia <niki@Nikis-MacBook-Pro.local>
2022-06-08 12:23:36 -1000

    first commit

--- src/components/article/article.js
+++ src/components/article/article.js
@@ -90,6 +90,11 @@ const data = [
   }
 ];
 
+function articleMaker(articleObj) {
+  const articleDiv = document.createElement('div');
+}
+
+
 /*
   Step 1: Write a component called 'articleMaker' to create an article.
   Your component is a function that takes an article object as its only argument,
